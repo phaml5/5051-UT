6445f069bb93c17738efc3bcc947524acc8225b6
Loi Pham <phaml5@seattleu.edu>
2019-06-06 23:36:22 -0700

    Added LogBackendTests.cs to the project

---
+++ UnitTests/Backend/LogBackendTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Backend;
+using HW1c.Models;
+using System.Linq;
+
+namespace UnitTests.Backend
+{
+    [TestClass]
+    public class LogBackendTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
