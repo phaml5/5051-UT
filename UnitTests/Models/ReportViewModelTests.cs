e5e9b166f2537907a570a1f2edc9fe1b2fdbbbe5
Loi Pham <phaml5@seattleu.edu>
2019-06-06 22:16:00 -0700

    Added ReportViewModel NumberOfUsers Set Test

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -31,5 +31,20 @@ namespace UnitTests.Models
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void ReportViewModel_NumberOfUsers_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+            var myInt = 3;
+
+            // Act
+            myTest.NumberOfUsers = myInt;
+            var result = myTest.NumberOfUsers;
+
+            // Assert
+            Assert.AreEqual(myInt, result);
+        }
     }
 }
