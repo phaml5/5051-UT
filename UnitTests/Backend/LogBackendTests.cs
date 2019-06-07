c3a1704f3a3791242303d44e66b98ba7dd08f63d
Loi Pham <phaml5@seattleu.edu>
2019-06-07 00:14:18 -0700

    Added LogBackend Create Valid Item Test

--- UnitTests/Backend/LogBackendTests.cs
+++ UnitTests/Backend/LogBackendTests.cs
@@ -20,5 +20,32 @@ namespace UnitTests.Backend
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void LogBackend_Create_Valid_Item_Should_Pass()
+        {
+            // Arange
+            var myData = LogBackend.Instance;
+
+            // Get the first item from the list
+            var oldItem = new LogModel();
+            oldItem.ID = "ID";
+            oldItem.PhoneID = "PhoneID";
+            oldItem.RecordedDateTime = DateTime.Parse("01/23/2019");
+            oldItem.Value = "Value";
+
+
+            // Act
+            var newItem = myData.Create(oldItem);
+
+            // Check each item one by one to ensure it is correctly loaded
+
+            // Assert
+            Assert.AreEqual(oldItem.ID, newItem.ID);
+            Assert.AreEqual(oldItem.PhoneID, newItem.PhoneID);
+            Assert.AreEqual(oldItem.RecordedDateTime, newItem.RecordedDateTime);
+            Assert.AreEqual(oldItem.Value, newItem.Value);
+        }
+
     }
 }
